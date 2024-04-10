5fa23caab2cb51f41d2b7fc58368f191b9fccf66
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2024-04-10 23:08:14 +0200

    feat: create nav routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_first_app/pages/library/library.dart';
 // import 'package:barcode_scan/barcode_scan.dart';
 
 void main() {
@@ -16,24 +17,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Insist',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFDC1A22)),
         useMaterial3: true,
       ),
+      initialRoute: '/home',
+      routes: {
+        // '/': (context) => Loading()
+        '/home': (context) => const MyHomePage(title: 'Insist Scanner'),
+        '/library': (context) => const Library()
+      },
       home: const MyHomePage(title: 'Insist Scanner'),
     );
   }
@@ -41,15 +33,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,120 +42,103 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  int _currentIndex=0;
-  String _scanBarcodeResult='';
+  int _currentIndex = 0;
+  String _scanBarcodeResult = '';
 
-  Future<void> scanBarcode() async{
+  Future<void> scanBarcode() async {
     String barcodeScanRes;
-    try{
-    barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-      '#ff6666', 'Annuler', true, ScanMode.BARCODE);
-    debugPrint(barcodeScanRes);
-    } on PlatformException{
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Annuler', true, ScanMode.BARCODE);
+      debugPrint(barcodeScanRes);
+    } on PlatformException {
       barcodeScanRes = 'Erreur Technique';
     }
-    if(!mounted) return;
-    
+    if (!mounted) return;
+
     setState(() {
       _scanBarcodeResult = barcodeScanRes;
     });
-
   }
+
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.primary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title,
-          style: TextStyle(color: Colors.white), ),
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_scanBarcodeResult)
-          ],
+          children: <Widget>[Text(_scanBarcodeResult)],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: this._currentIndex,
-        items: [
-          BottomNavigationBarItem(
-          icon: const Icon(Icons.home_filled),
-          label:'Home',
-          backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer
-          ),
-          BottomNavigationBarItem(
-          icon: const Icon(Icons.explore),
-          label:'Explore',
-          backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer
-          ),
-          BottomNavigationBarItem(
-          icon: const Icon(Icons.web_stories),
-          label:'Library',
-          backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer
-          ),
-
-          BottomNavigationBarItem(
-          icon: const Icon(Icons.person),
-          label:'Profil',
-          backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer
-          ),
-          BottomNavigationBarItem(
-          icon: const Icon(Icons.more_vert),
-          label:'More',
-          backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer
-          ),
+          type: BottomNavigationBarType.fixed,
+          currentIndex: this._currentIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.home_filled),
+                label: 'Home',
+                backgroundColor:
+                    Theme.of(context).colorScheme.onPrimaryContainer),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.explore),
+                label: 'Explore',
+                backgroundColor:
+                    Theme.of(context).colorScheme.onPrimaryContainer),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.web_stories),
+                label: 'Library',
+                backgroundColor:
+                    Theme.of(context).colorScheme.onPrimaryContainer),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.person),
+                label: 'Profil',
+                backgroundColor:
+                    Theme.of(context).colorScheme.onPrimaryContainer),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.more_vert),
+                label: 'More',
+                backgroundColor:
+                    Theme.of(context).colorScheme.onPrimaryContainer),
           ],
-          onTap:(index){
+          onTap: (index) {
             setState(() {
-              _currentIndex=index;
+              _currentIndex = index;
+              switch (index) {
+                case 0:
+                  Navigator.pushNamed(context, "/home");
+                  break;
+                case 1:
+                  Navigator.pushNamed(context, "/explore");
+                  break;
+                case 2:
+                  Navigator.pushNamed(context, "/library");
+                  break;
+                case 3:
+                  Navigator.pushNamed(context, "/profil");
+                  break;
+                case 4:
+                  Navigator.pushNamed(context, "/more");
+                  break;
+              }
             });
-          }
-        ),
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: scanBarcode,
         tooltip: 'Scan',
         backgroundColor: Theme.of(context).colorScheme.primary,
-
         child: const Icon(Icons.fullscreen, color: Colors.white),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
