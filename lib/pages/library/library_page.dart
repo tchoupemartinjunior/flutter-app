5f56613d3818df239227eb97e90e4f3c329c18d1
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2024-04-14 22:42:21 +0200

    search bar and book card v1

--- lib/pages/library/library_page.dart
+++ lib/pages/library/library_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 export '../library/library_page.dart';
 
 class LibraryPage extends StatefulWidget {
@@ -11,12 +14,39 @@ class LibraryPage extends StatefulWidget {
 class _LibraryPageState extends State<LibraryPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text("Library")],
+    return Scaffold(
+      body: Column(
+        children:<Widget>[
+          Container(
+            margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+            child:TextField(
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: 'Book Title',
+                border:OutlineInputBorder(
+                  borderRadius:BorderRadius.circular(10),
+                  borderSide: const BorderSide(color:Colors.grey)
+                )
+              ),
+            )
+          ),
+          Expanded(
+        child: ListView(
+          children:const [
+            Card(
+              child: ListTile(
+                leading:FlutterLogo(size:56.0),
+                title: Text('Le Petit Prince'),
+                subtitle: Text('Antoine de st Exupery'),
+                isThreeLine: true,
+                trailing: Icon(Icons.more_vert)
+              )
+            )
+          ],
         ),
+          )
+        ]
+
       ),
     );
   }
